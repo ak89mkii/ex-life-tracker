57bb49d8f22960b21fbf6f37b74431f078b8b7cf
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2021-04-15 01:26:40 -0400

    README image

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 import Title from './components/Title/Title.jsx'
 import Options from './components/Options/Options.jsx'
-import Stage from './components/Stage/Stage.jsx'
-import HP from './components/HP/HP.jsx';
-import EX from './components/EX/EX.jsx';
 
 class App extends Component {
   state = {
