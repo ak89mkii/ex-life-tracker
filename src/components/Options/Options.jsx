734f558338d5b1460346673c3f8b46d1bb6081ba
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2021-04-26 16:06:29 -0400

    Added instructions to Options.

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -14,9 +14,9 @@ class Options extends Component {
 
     }
 
-    state = {
-        options: !false,
-    }
+    // state = {
+    //     options: !false,
+    // }
 
     optionsOpen = () => this.setState({ options: true })
     optionsClose = () => this.setState({ options: false })
